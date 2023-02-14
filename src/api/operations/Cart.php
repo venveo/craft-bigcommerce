f8cdc97c84067ef5b1b76b4b735a0783116868b5
Ransom Roberson <ransom@venveo.com>
2023-02-14 14:50:16 -0500

    Delete invalid cart cookies

--- src/api/operations/Cart.php
+++ src/api/operations/Cart.php
@@ -2,6 +2,7 @@
 
 namespace venveo\bigcommerce\api\operations;
 
+use GuzzleHttp\Exception\ClientException;
 use venveo\bigcommerce\base\ApiOperationInterface;
 use venveo\bigcommerce\Plugin;
 use yii\web\Cookie;
@@ -16,7 +17,14 @@ class Cart implements ApiOperationInterface
         $channelId = Plugin::getInstance()->settings->getDefaultChannel(true);
         $cart = null;
         if ($cartCookie) {
-            $cart = $client->cart($cartCookie)->get()?->getCart();
+            try {
+                $cart = $client->cart($cartCookie)->get()?->getCart();
+            } catch (ClientException) {
+                if (\Craft::$app->request instanceof Request) {
+                    \Craft::$app->response->cookies->remove('bc_cartId');
+                }
+                $cart = null;
+            }
         }
         if (!$cart && $create) {
             $customerId = Customer::getCurrentCustomerId();
