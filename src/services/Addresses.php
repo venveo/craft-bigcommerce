1f6c7106e6c029cf26752d737c924b6c23852046
Ransom Roberson <ransom@venveo.com>
2023-02-24 21:29:36 -0500

    Added methods for retrieving countries & states

--- src/services/Addresses.php
+++ src/services/Addresses.php
@@ -4,12 +4,17 @@ namespace venveo\bigcommerce\services;
 
 use BigCommerce\ApiV3\ResourceModels\Customer\CustomerAddress;
 use craft\base\Component;
+use craft\helpers\Json;
+use GuzzleHttp\RequestOptions;
+use Illuminate\Support\Collection;
 use venveo\bigcommerce\api\operations\Customer;
-use venveo\bigcommerce\Plugin;
+use venveo\bigcommerce\base\SdkClientTrait;
 use yii\web\ForbiddenHttpException;
 
 class Addresses extends Component
 {
+    use SdkClientTrait;
+
     /**
      * @return \BigCommerce\ApiV3\ResourceModels\Customer\CustomerAddress[]
      */
@@ -24,17 +29,46 @@ class Addresses extends Component
             $filters['id:in'] = $addressId;
         }
 
-        return Plugin::getInstance()->getApi()->getClient()->customers()->addresses()->getAll($filters)->getAddresses();
+        return $this->getClient()->customers()->addresses()->getAll($filters)->getAddresses();
     }
 
-    public function saveAddress(CustomerAddress $address, int $addressId = null): \BigCommerce\ApiV3\ResponseModels\Customer\CustomerAddressesResponse
-    {
+    public function saveAddress(
+        CustomerAddress $address,
+        int $addressId = null
+    ): \BigCommerce\ApiV3\ResponseModels\Customer\CustomerAddressesResponse {
         $isNew = $addressId === null;
         if ($isNew) {
-            $result = Plugin::getInstance()->getApi()->getClient()->customers()->addresses()->create([$address]);
+            $result = $this->getClient()->customers()->addresses()->create([$address]);
         } else {
-            $result = Plugin::getInstance()->getApi()->getClient()->customers()->addresses()->update([$address]);
+            $result = $this->getClient()->customers()->addresses()->update([$address]);
         }
         return $result;
+    }
+
+    public function getCountries(): Collection
+    {
+        $countries = new Collection(Json::decodeIfJson($this->getV2Client()->getRestClient()->get('countries', [
+            RequestOptions::QUERY => [
+                'page' => 1,
+                'limit' => 250,
+            ]
+        ])->getBody()->getContents()));
+        return $countries;
+    }
+
+    public function getStates(?int $countryId = null): Collection
+    {
+        $uri = 'countries/states';
+        if ($countryId) {
+            $uri = "countries/$countryId/states";
+        }
+        $allStates = new Collection(Json::decodeIfJson($this->getV2Client()->getRestClient()->get($uri, [
+            RequestOptions::QUERY => [
+                'page' => 1,
+                'limit' => 250,
+            ]
+        ])->getBody()->getContents()));
+
+        return $allStates;
     }
 }
